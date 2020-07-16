9f150274e787c6e3da6640a7c35d98f1744e43fb
kshitij Parashar <kshitijsharma0684@gmail.com>
2020-07-16 15:52:48 +0530

    accounts/urls.py updated

--- assignment/accounts/urls.py
+++ assignment/accounts/urls.py
@@ -2,11 +2,13 @@
 from django.urls import path
 from . models import User
 from . serializers import UserSerializer
-from . resource_api import ResourceAPIView 
+from . resource_api import ResourceAPIView
+from .generic_resource_api import * 
 
 
 
 urlpatterns = [
 	path('api/registration/<int:pk>', ResourceAPIView.as_view(model=User,resource_serializer=UserSerializer)),
+	path('api/POST/registration/<int:pk>', ResourceCreateAPIView.as_view(model=User,resource_serializer=UserSerializer)),
 	
 	]
